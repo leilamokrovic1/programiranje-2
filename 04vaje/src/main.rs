6c374767c2588923beae5ec87ad1a8daba58cccd
leilamokrovic1 <mokrovicleila@gmail.com>
2024-05-13 14:18:01 +0200

    Dodajanje sprememb

---
+++ 04vaje/src/main.rs
@@ -0,0 +1,100 @@
+// Napišite različne funkcije, ki kot argument sprejmejo zaprtje (closure) in ga pokličejo z različnimi argumenti, ki ustrezajo sledečim ocaml tipom:
+// Tip zaprtja naj bo čim bolj splošen (Fn, FnOnce, FnMut).
+//  (int -> int) -> int -> int
+//  ('a -> 'b) -> 'a -> 'b
+//  ('a -> 'a -> 'b) -> 'a -> 'a -> 'b
+//  map: ('a -> 'b) -> 'a list -> 'b list  (Uporabite Vec<T> namesto list, predpostavite, da funkcija ne spremeni elementov seznama)
+//  map_and_mutate: ('a -> 'b) -> 'a list -> 'b list // Definirajte funkcijo tako, da lahko zaprtje spremeni elemente seznama
+//  ponavljaj: int -> ('a -> 'a) -> 'a -> 'a // Ponovi zaprtje n-krat
+//  filter: ('a -> bool) -> 'a list -> 'a list // Vrne seznam elementov, ki zadoščajo pogoju - uporabite Vec<T> namesto list in že vgrajeno funkcijo filter
+
+// Vzemite zaporedja iz prejšnjih vaj in naredite nov objekt, ki sprejme zaporedje in ga naredi iterabilnega
+
+// Iteratorji
+
+// Napišite funkcijo, ki sprejme vektor XYZ in s pomočjo iteratorja naredi W
+// števil in izpiše vsako v svojo vrstico
+// nizov in izpiše njihove dolžine
+// nizov in vrne vsoto njihovih dolžin
+// vektor parov (i32, i32) in vrne vsoto njihovih pozitivnih produktov
+// dva vektorja <i32> in vrne vektor, ki vsebuje vsote parov
+// dva vektorja <i32> in vrne vsoto poparjenih pozitivni produktov s pomočjo ene izmed prejšnjih nalog
+// vektor Option<T> in izpiše vse T-je
+// vektor Option<T> in vrne število Some-ov
+// odfiltrira števila deljena s 3
+
+fn print_stevila_vektorja(vector: &[i32]) {
+    for stevilo in vector.iter() {
+        println!({},"stevilo");
+    }
+}
+
+
+fn print_nizi_vektorja(vector: &[str]) {
+    for niz in vector.iter() {
+        println!({},niz.len());
+    }
+}
+
+fn vsota_dolzin_nizov(vector: [&str]) {
+    vector.iter()
+}
+
+// Dopolnite spodnjo funkcijo, da vrne niz, kjer so vse prve črke posameznih besed velike
+// ["Just,", " ", "hello", " ", "world", "!"] -> "Just, Hello World", "!"
+pub fn capitalize_words_string(words: &[&str]) -> String {
+    panic!("Not implemented");
+}
+// Napišite funkcijo `fakulteta`, ki izračuna fakulteto števila n. Uporabite iteratorje (torej brez lastne for zanke, rekurzije)
+// Namig: fold, reduce, `..`...
+
+// -------------------------------------------------------------------------------------------------
+/// Dodatno:
+// Koda vzeta iz googlvih rust vaj:
+// Vse se da lepo narediti samo z iteratorji (brez indeksov, brez for zank, brez mutabilnosti)
+/*
+/// Calculate the differences between elements of `values` offset by `offset`,
+/// wrapping around from the end of `values` to the beginning.
+///
+/// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
+fn offset_differences<N>(offset: usize, values: Vec<N>) -> Vec<N>
+where
+    N: Copy + std::ops::Sub<Output = N>,
+{
+    unimplemented!()
+}
+
+#[test]
+fn test_offset_one() {
+    assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+    assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
+    assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
+}
+
+#[test]
+fn test_larger_offsets() {
+    assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
+    assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
+    assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
+    assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+}
+
+#[test]
+fn test_custom_type() {
+    assert_eq!(
+        offset_differences(1, vec![1.0, 11.0, 5.0, 0.0]),
+        vec![10.0, -6.0, -5.0, 1.0]
+    );
+}
+
+#[test]
+fn test_degenerate_cases() {
+    assert_eq!(offset_differences(1, vec![0]), vec![0]);
+    assert_eq!(offset_differences(1, vec![1]), vec![0]);
+    let empty: Vec<i32> = vec![];
+    assert_eq!(offset_differences(1, empty), vec![]);
+}
+
+
+
+*/
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,26 @@
-use rand::Rng;
+// Napišite različne funkcije, ki kot argument sprejmejo zaprtje (closure) in ga pokličejo z različnimi argumenti, ki ustrezajo sledečim ocaml tipom:
+// Tip zaprtja naj bo čim bolj splošen (Fn, FnOnce, FnMut).
+//  (int -> int) -> int -> int
+//  ('a -> 'b) -> 'a -> 'b
+//  ('a -> 'a -> 'b) -> 'a -> 'a -> 'b
+//  map: ('a -> 'b) -> 'a list -> 'b list  (Uporabite Vec<T> namesto list, predpostavite, da funkcija ne spremeni elementov seznama)
+//  map_and_mutate: ('a -> 'b) -> 'a list -> 'b list // Definirajte funkcijo tako, da lahko zaprtje spremeni elemente seznama
+//  ponavljaj: int -> ('a -> 'a) -> 'a -> 'a // Ponovi zaprtje n-krat
+//  filter: ('a -> bool) -> 'a list -> 'a list // Vrne seznam elementov, ki zadoščajo pogoju - uporabite Vec<T> namesto list in že vgrajeno funkcijo filter
 
+// Vzemite zaporedja iz prejšnjih vaj in naredite nov objekt, ki sprejme zaporedje in ga naredi iterabilnega
+//4. VAJE
 
-fn main() {
-    println!("5={}", five());
+// fn apply <F: FnOnce(i32) -> i32>(func: F, a: i32) -> i32 {
+//     func(a)
+// }
 
-}
+// fn apply_b <T1, T2, F: FnOnce(T1) -> T2>(func: F, a: T1) -> T2 {
+//     func(a)
+// }
 
-fn five() -> i32 {
-     5
-}
+// fn main() {
+//    // let k_dva = |x| {x * 2};
+//     let neki = 12;
+//     println!("apply {}",apply(|x| x * neki, 10));
+//     println!("apply_b {}",apply_b(|x| x * neki, 10))
